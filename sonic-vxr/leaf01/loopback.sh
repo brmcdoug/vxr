800dd82162409b89b97f88d1c23fd9eee2390693
brmcdoug <brmcdoug@cisco.com>
2026-04-01 13:20:03 -0700

    pw

--- sonic-vxr/leaf01/loopback.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo config interface ip add Loopback0 20.0.0.3/32
-sudo config interface ip add Loopback0 fc00:0:2003::1/128
--- sonic-vxr/spine00/loopback.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo config interface ip add Loopback0 20.0.0.1/32
-sudo config interface ip add Loopback0 fc00:0:2001::1/128
